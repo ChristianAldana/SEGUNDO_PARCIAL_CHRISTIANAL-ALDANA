29fd7d551bda66147f85c4f59e00071bfe616e89
Christian <caldanas2@miumg.edu.gt>
2022-05-01 23:04:53 -0600

    Getter and setter de la figura geometrica perimetro

--- perimetro.java
+++ perimetro.java
@@ -52,7 +52,71 @@ public class perimetro {
          
     } 
 
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPer() {
+        return per;
+    }
+
+    public void setPer(double per) {
+        this.per = per;
+    }
+
+    public double getN1() {
+        return n1;
+    }
+
+    public void setN1(double n1) {
+        this.n1 = n1;
+    }
+
+    public double getN2() {
+        return n2;
+    }
+
+    public void setN2(double n2) {
+        this.n2 = n2;
+    }
+
+    public double getPerim() {
+        return perim;
+    }
+
+    public void setPerim(double perim) {
+        this.perim = perim;
+    }
+
+    public double getPerimetroTriangulo() {
+        return perimetroTriangulo;
+    }
+
+    public void setPerimetroTriangulo(double perimetroTriangulo) {
+        this.perimetroTriangulo = perimetroTriangulo;
+    }
+
+    public double getPerimetro1() {
+        return perimetro1;
+    }
+
+    public void setPerimetro1(double perimetro1) {
+        this.perimetro1 = perimetro1;
+    }
     
+         
          
          
 }
